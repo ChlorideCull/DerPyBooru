4d3900371974d2d0d4408cd96e4c2a39d132fb21
Chloride Cull <chloride@devurandom.net>
2016-01-06 21:45:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,25 +6,24 @@ from setuptools import find_packages
 import derpibooru
 
 setup(
-  name = "DerPyBooru",
-  description = "Python bindings for Derpibooru's API",
-  url = "https://github.com/joshua-stone/DerPyBooru",
-  version = "0.6",
-  author = "Joshua Stone",
-  author_email = "joshua.gage.stone@gmail.com",
+  name = "DerPyBooru-NonStandard",
+  description = "Python bindings for Derpibooru's API - Non-Standard Modifications",
+  url = "https://github.com/ChlorideCull/DerPyBooru",
+  version = "0.6-1",
+  author = "Joshua Stone, Chloride Cull",
+  author_email = "steamruler@gmail.com",
   license = "Simplified BSD License",
   platforms = ["any"],
   packages = find_packages(),
   install_requires = ["requests"],
   include_package_data = True,
-  download_url = "https://github.com/joshua-stone/DerPyBooru/tarball/0.6",
+  download_url = "https://github.com/ChlorideCull/DerPyBooru/archive/non-standard-changes.tar.gz",
   classifiers = [
     "Development Status :: 4 - Beta",
     "Intended Audience :: Developers",
     "Operating System :: OS Independent",
     "License :: OSI Approved :: BSD License",
     "Topic :: Software Development :: Libraries :: Python Modules",
-    "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3"
   ]
 )
